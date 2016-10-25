8598650ee6f8a194fb6ce425ce9fa891bd4eb83f
Xavier <xcoleman86@outlook.com>
2016-10-24 20:13:45 -0500

    deleted form element

--- imports/ui/pages/index.js
+++ imports/ui/pages/index.js
@@ -5,9 +5,6 @@ export const Index = () => (
 <Jumbotron className="text-center">
   <h2>Character creation</h2>
 <p>You will need to create a character in order to track stats in the game.</p>
-<form onSubmit={}>
-  <input type="text" ref="resolution" placeholder="finish react meteor sries" />
-  </form>
   <p><a className="btn btn-success" href="https://themeteorchef.com/base" role="button">Read the Documentation</a></p>
 </Jumbotron>
 );
